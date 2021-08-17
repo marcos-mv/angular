aff1f38569247a7210f721a81897650efd1fb103
marcos-mv <vinicius_kof36@hotmail.com>
2021-08-17 16:18:32 -0300

    Adding Angular first component.

--- begin_angular/firstapp/src/app/app.component.ts
+++ begin_angular/firstapp/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstapp';
+  public initialCount = 100;
+
+  public clicked = false;
+
+  public active = '';
+
+  public button_click() {
+    if (!this.clicked) {
+      this.initialCount++;
+      this.active = 'liked'
+      console.log('clicked')
+    }
+    else {
+      this.initialCount--;
+      this.active = ''
+      console.log('undo click')
+    }
+    this.clicked = !this.clicked; //inicia falso e toda vez que alguem clicar no botao inverte seu estado
+  }
 }
